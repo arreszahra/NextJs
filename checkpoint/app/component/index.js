f90c32c147d492020ee09c5da9287df14de323bc
Zahra Arres <arreszahra0@gmail.com>
2024-07-11 11:47:53 +0100

    first commit

---
+++ checkpoint/app/component/index.js
@@ -0,0 +1,15 @@
+import styles from './index.module.css'
+
+function nextJs () {
+    return ( 
+ <div className={styles.tout}>
+    <div >
+    <span>
+        something
+    </span>
+    </div>
+ </div>
+    )
+    
+}
+export default nextJs
